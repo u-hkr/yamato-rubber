87660ae8bdaf6d53d53ce8dfc3d5c9a75ae3c6f3
u-hkr <ukari.uzuhashi@gmail.com>
2025-12-13 15:36:17 +0900

    トップページ

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,5 +15,16 @@ $(window).on('load', function() {
 });
 
 $(function(){
-
+  $("#bg").slick({
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 60000,
+    cssEase: "linear",
+    slidesToShow: 1,
+    variableWidth: true,
+    swipe: false,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+  })
 });
