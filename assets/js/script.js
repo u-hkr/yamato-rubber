a1720bbea9013b1ba90a6372877fe0b42b784681
u-hkr <ukari.uzuhashi@gmail.com>
2025-12-30 15:35:47 +0900

    ヘッダー

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,18 @@
 // ローディング
 $(window).on('load', function() {
   var $loading = $("#loading");
-  const is_firstload = sessionStorage.getItem('is-first-load');
+  var is_firstload = sessionStorage.getItem('is-first-load');
   if (!is_firstload) {
     sessionStorage.setItem('is-first-load', true);
+    $loading.addClass("active");
+
     setTimeout(function(){
       $loading.fadeOut();
+      $("#mv").addClass("active");
     },3000);
   } else {
     $loading.hide();
+    $("#mv").addClass("active");
   }
 });
 
